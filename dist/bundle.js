1b560ea1e0519021c44c0c4adac6d0836fb8fd46
Roger Cornet <roger.cornet@netcentric.biz>
2018-01-22 23:01:27 +0100

    correct markup and js

--- dist/bundle.js
+++ dist/bundle.js
@@ -68,20 +68,25 @@
 /***/ (function(module, exports) {
 
 window.onload = function () {
+  var $form = document.querySelector('form');
   var $fieldset = document.querySelector('fieldset');
   var $input = document.querySelector('input');
-  var $legend = document.createElement('legend');
   var $label = document.createElement('label');
+  var $legend = document.createElement('legend');
+  var $submitButton = document.createElement('button');
 
-  $legend.innerHTML = "User fieldset";
-  $label.innerHTML = "User name";
+  $legend.innerHTML = 'User form';
+  $label.innerHTML = 'User name';
+  $submitButton.innerHTML = 'Submit'
+  
+  $submitButton.setAttribute('type', 'submit');
   $label.setAttribute('for', 'username');
   $input.setAttribute('id', 'username');
-
-  $fieldset.appendChild($legend);
-  $fieldset.appendChild($label);
-
   $input.setAttribute('name', 'username');
+
+  $fieldset.insertBefore($label, $input); 
+  $fieldset.insertBefore($legend, $label); 
+  $form.appendChild($submitButton);
 }
 
 
--- lint-a11y.js
+++ lint-a11y.js
@@ -12,6 +12,11 @@ const test = pa11y(pa11yConfig)
 
 paths.a11y.forEach((url) => {
   test.run(url, (error, results) => {
+    if (error) {
+      console.error(error.message)
+      process.exit(-1)
+    }
+
     if (results.length > 0) {
       // Print errors and finish the process
       console.error(JSON.stringify(results, null, `  `))
